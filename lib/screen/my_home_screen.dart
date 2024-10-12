251a48fe1ebee31379bd9b89ae4051728e3e6294
Anna-Myzukina <anna.muzykina83@gmail.com>
2024-10-12 16:48:49 +0200

    add screens to create, view, update posts

--- lib/screen/my_home_screen.dart
+++ lib/screen/my_home_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_devcamp_api/screen/create_post_screen.dart';
+import 'package:flutter_devcamp_api/screen/view_post_screen.dart';
 
 class MyHomeScreen extends StatelessWidget {
   const MyHomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Posts App')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const ViewPostsScreen(),
+                  ),
+                );
+              },
+              child: const Text('View Posts'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const CreatePostScreen(),
+                  ),
+                );
+              },
+              child: const Text('Create Post'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/screen/view_post_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_devcamp_api/screen/update_post_screen.dart';
+import '../normal_http/api/post_service.dart';
+import '../normal_http/model/post.dart';
+
+class ViewPostsScreen extends StatefulWidget {
+  const ViewPostsScreen({super.key});
+
+  @override
+  State<ViewPostsScreen> createState() => _ViewPostsScreenState();
+}
+
+class _ViewPostsScreenState extends State<ViewPostsScreen> {
+  final PostService postService = PostService();
+  late Future<List<Post>> futurePosts;
+
+  @override
+  void initState() {
+    super.initState();
+    futurePosts = postService.fetchPost();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('All Posts')),
+      body: FutureBuilder<List<Post>>(
+        future: futurePosts,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Error loading posts'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No posts available'));
+          } else {
+            final posts = snapshot.data!;
+            return ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                final post = posts[index];
+                return ListTile(
+                  title: Text(post.title),
+                  subtitle: Text(post.body),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.edit),
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) =>  UpdatePostScreen(post:post),
+                            ),
+                          );
+                        },
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () async {
+                          try {
+                            await postService.deletePost(post.id);
+                            setState(() {
+                              futurePosts = postService.fetchPost();
+                            });
+                          } catch (e) {
+                            debugPrint(e.toString());
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
